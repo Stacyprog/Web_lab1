3913d1d1f77615628082281907157837a47e0f75
Stacyprog <54043006+Stacyprog@users.noreply.github.com>
2019-08-14 22:34:11 +0300

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,21 +1,34 @@
 function formValidation()
 {
     var y = document.information.y;
-    if(y_validation(y,-3,5))
-    {
+    var x = document.information.clicked_x;
+    if(y_validation(y,-3,5)) {
         if (r_validation()) {
-
+            if (x_validation(x)) {
+                return true;
+            }
         }
     }
-    return false;
+    else return false;
 
+}
+
+function x_validation(x) {
+    var x_len = x.value.length;
+    var x_value = x.value;
+    if (x_len == 0 || isNaN(x_value)) {
+        alert("Введите значение X");
+        x.focus();
+        return false;
+    }
+    return true;
 }
 
 function y_validation(y,min,max)
 {
     var y_value = y.value.replace(",", ".");
     var y_len = y.value.length;
-    if (y_value < min || y_value > max || y_len == 0 || isNaN(y_value)) 
+    if (y_value < min || y_value > max || y_len == 0 || isNaN(y_value))
     {
         alert("Введите значение Y от -3 до 5.");
         y.focus();
@@ -38,7 +51,8 @@ function r_validation(){
     }
     if(!radioValue){
         alert("Пожалуйста, выберите значение R");
+        radios.focus();
         return false;
     }
-
+    return true;
 }
